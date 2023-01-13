8184ccd35b9a0b6ebf41bd478c429bd61707df40
Tishko1 <113176690+Tishko1@users.noreply.github.com>
2023-01-13 15:41:16 +0300

    remove comments

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,47 +1,17 @@
 'use strict';
 
 export const fetchCountries = name => {
-
   const BASE_URL = 'https://restcountries.com/v3.1';
-  
-  return fetch(`${BASE_URL}/name/${name}?fields=name,capital,population,flags,languages`
-  ) .then(response => {
-      if (!response.ok) {
-        if (response.status === 404) {
-          return [];
-        }
-        throw new Error(response.status);
+
+  return fetch(
+    `${BASE_URL}/name/${name}?fields=name,capital,population,flags,languages`
+  ).then(response => {
+    if (!response.ok) {
+      if (response.status === 404) {
+        return [];
       }
-      return response.json();
-    })
-    
+      throw new Error(response.status);
+    }
+    return response.json();
+  });
 };
-
-
-// `https://restcountries.com/v3.1/name/${name}?fields=,name,capital,population,flags,languages`
-
-
-// export class JsonPlaceholderApi {
-//   static BASE_URL = 'https://jsonplaceholder.typicode.com';
-
-//   constructor() {
-//     this.page = 1;
-//   }
-
-//   fetchPosts() {
-//     const searchParams = new URLSearchParams({
-//       _limit: '20',
-//       _page: this.page,
-//     });
-
-//     return fetch(`${JsonPlaceholderApi.BASE_URL}/posts?${searchParams}`).then(
-//       response => {
-//         if (!response.ok) {
-//           throw new Error(response.status);
-//         }
-
-//         return response.json();
-//       }
-//     );
-//   }
-// }
--- src/index.js
+++ src/index.js
@@ -8,34 +8,32 @@ const countryList = document.querySelector('.country-list');
 const countryInfo = document.querySelector('.country-info');
 const DEBOUNCE_DELAY = 300;
 
-input.addEventListener('input',
+input.addEventListener(
+  'input',
   debounce(e => {
     const trimmedValue = input.value.trim();
     cleanHtml();
-    
+
     if (trimmedValue !== '') {
       fetchCountries(trimmedValue)
-      .then(foundData => {
-        if (foundData.length > 10) {
-          Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
-        } else if (foundData.length === 0) {
-          Notiflix.Notify.failure('Oops, there is no country with that name');
-        } else if (foundData.length >= 2 && foundData.length <= 10) {
-          renderCountryList(foundData);
-        } else if (foundData.length === 1) {
-          renderOneCountry(foundData);
-        }
-      }).catch(error => {
-        console.error(error);
-      })
-      // .finally(() => {
-      //   setTimeout(() => {
-      //   input.value = '';
-      // }, 3000)
-      // });
-    } 
+        .then(foundData => {
+          if (foundData.length > 10) {
+            Notiflix.Notify.info(
+              'Too many matches found. Please enter a more specific name.'
+            );
+          } else if (foundData.length === 0) {
+            Notiflix.Notify.failure('Oops, there is no country with that name');
+          } else if (foundData.length >= 2 && foundData.length <= 10) {
+            renderCountryList(foundData);
+          } else if (foundData.length === 1) {
+            renderOneCountry(foundData);
+          }
+        })
+        .catch(error => {
+          console.error(error);
+        });
+    }
   }, DEBOUNCE_DELAY)
-  
 );
 
 function renderCountryList(countries) {
@@ -55,7 +53,9 @@ function renderOneCountry(countries) {
   const markup = countries
     .map(country => {
       return `<li>
-            <img src="${country.flags.svg}" alt="Flag of ${country.name.official}" width="30" hight="20">
+            <img src="${country.flags.svg}" alt="Flag of ${
+        country.name.official
+      }" width="30" hight="20">
             <b>${country.name.official}</b>
             <p><b>Capital</b>: ${country.capital}</p>
             <p><b>Population</b>: ${country.population}</p>
@@ -67,6 +67,6 @@ function renderOneCountry(countries) {
 }
 
 function cleanHtml() {
-  countryList.innerHTML='';
-  countryInfo.innerHTML='';
+  countryList.innerHTML = '';
+  countryInfo.innerHTML = '';
 }
